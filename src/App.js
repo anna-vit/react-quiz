f81aa925a91439fab61212f4144d066c585a4e3d
anna-vit <sidorovahanna795@gmail.com>
2024-01-22 13:47:15 -0500

    Handing Loading,Error,Ready status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 import { useEffect, useReducer } from "react";
 
 const initialState = {
@@ -24,7 +27,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -37,8 +42,9 @@ export default function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
