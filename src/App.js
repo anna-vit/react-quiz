914d4be9e961cdd724cac81739cb2a8ac95223e2
anna-vit <sidorovahanna795@gmail.com>
2024-01-22 12:44:30 -0500

    loading questions from fake API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  //   'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFaild":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFaild" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
